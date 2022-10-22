b118b65e61d89b379ceabacedcf0a05633d8485a
Westly <93689946+WestlyDust@users.noreply.github.com>
2022-10-22 16:53:31 -0500

    Add files via upload

---
+++ Load.js
@@ -0,0 +1,19 @@
+
+function injectScript(src) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = src;
+        script.addEventListener('load', resolve);
+        script.addEventListener('error', e => reject(e.error));
+        document.head.appendChild(script);
+    });
+}
+
+injectScript('https://cdn.jsdelivr.net/gh/WestlyDust/ChoiceScript_Stats_Modifier@28a1f51b8e0fa72b8e990e41d818250e2b1b98cb/Cheat.js')
+    .then(() => {
+        console.log('Script loaded!');
+        var btns = document.getElementById("buttons");
+        btns.innerHTML = btns.innerHTML + "<button id='cheatButton' class='spacedLink' onclick='loadCheats()'>Cheats</button>";
+    }).catch(error => {
+        console.error(error);
+    });
