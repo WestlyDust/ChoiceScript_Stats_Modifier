bb17ada5b537b17fdf3cb163b6bd81b821969e5d
Westly <93689946+WestlyDust@users.noreply.github.com>
2023-03-08 20:03:56 -0600

    Add files via upload

--- CheatSelectable/Load.js
+++ CheatSelectable/Load.js
@@ -10,10 +10,7 @@ function injectScript(src) {
 
 injectScript('Init.js')
     .then(() => {
-        console.log('Script loaded!');
-        var btns = document.getElementById("buttons");
-        btns.innerHTML = btns.innerHTML + "<button id='cheatButton' class='spacedLink' onclick='loadCheats()'>Cheats</button>";
-        btns.innerHTML = btns.innerHTML + "<button id='reloadButton' class='spacedLink' onclick='location.reload()'>Reload</button>";
+        console.log('Initialized');
     }).catch(error => {
         console.error(error);
     });
