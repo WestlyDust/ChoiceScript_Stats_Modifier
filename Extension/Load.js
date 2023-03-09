1e80841dfa94f1e6aeecd102f8ffe5c6734cd858
Westly <93689946+WestlyDust@users.noreply.github.com>
2023-03-09 12:27:11 -0600

    Add files via upload

---
+++ Extension/Load.js
@@ -0,0 +1,16 @@
+function injectScript(src) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = src;
+        script.addEventListener('load', resolve);
+        script.addEventListener('error', e => reject(e.error));
+        document.head.appendChild(script);
+    });
+}
+
+injectScript('https://cdn.jsdelivr.net/gh/WestlyDust/ChoiceScript_Stats_Modifier@Main/CheatSelectable/InitCheats.js')
+    .then(() => {
+        console.log('Initialized');
+    }).catch(error => {
+        console.error(error);
+    });
