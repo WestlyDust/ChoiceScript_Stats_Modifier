9e4d9383335d9cf6bfff57b9157c07d9ee9cbfb3
Westly <93689946+WestlyDust@users.noreply.github.com>
2023-03-11 23:28:51 -0600

    Add files via upload

---
+++ CheatStatCharts/Load.js
@@ -0,0 +1,18 @@
+function injectScript(src) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = src;
+        script.addEventListener('load', resolve);
+        script.addEventListener('error', e => reject(e.error));
+        document.head.appendChild(script);
+    });
+}
+
+injectScript('https://cdn.jsdelivr.net/gh/WestlyDust/ChoiceScript_Stats_Modifier@Main/CheatStatCharts/Cheat.js')
+    .then(() => {
+        console.log('Cheats loaded!');
+        var btns = document.getElementById("buttons");
+        btns.innerHTML = btns.innerHTML + "<button id='cheatButton' class='spacedLink' onclick='loadCheats()'>Modify Stats</button>";
+    }).catch(error => {
+        console.error(error);
+    });
